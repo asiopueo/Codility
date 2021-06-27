3e2a1ab68b8695715cf07ed78fdceddc3784441c
Martin <martin.lippl@pueo.de>
2021-06-27 19:46:20 +0200

    Brute-force solution of PassingCars

---
+++ PrefixSums/PassingCars/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace PassingCars
+{
+    class Solution 
+    {
+        // This seems to be the brute-force solution, O(N^2) time complexity.
+        public static int solution(int[] A) 
+        {
+            int counter = 0;
+
+            for (int i=0; i<A.Length; i++)
+            {
+                if (A[i] == 0)
+                {
+                    for (int j=i+1; j<A.Length; j++)
+                    {
+                        if (A[j] == 1)
+                        {
+                            counter++;
+                        }
+                    }
+                }
+            }
+
+            return counter;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //int[] array = new int[] {0, 1, 0, 1, 1};
+            int[] array = new int[] {0, 1, 0, 1, 1, 0, 1};
+
+            int result = Solution.solution(array);
+
+            Console.WriteLine($"The solution is {result}.");
+        }
+    }
+}
