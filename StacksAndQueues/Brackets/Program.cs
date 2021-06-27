7a3f9a785e147c1eb65ece71eb256ef4c373fac8
Martin <martin.lippl@pueo.de>
2021-06-27 18:38:21 +0200

    Finished 'Brackets'-task

---
+++ StacksAndQueues/Brackets/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Brackets
+{
+    class Solution
+    {
+        public static int solution(string S)
+        {
+            char[] str = S.ToCharArray();
+            Stack<char> stack = new Stack<char>();
+            
+            for (int i=0; i<S.Length; i++)
+            {
+                                
+                if (str[i] == '(' || str[i] == '[')
+                    stack.Push(str[i]);
+                else if (stack.Count != 0) {
+                    char lastCharacter = stack.Peek();    
+                    if (str[i] == ')' && lastCharacter == '(' || str[i] == ']' && lastCharacter == '[')
+                        stack.Pop();
+                    else
+                        return 0;
+                }
+                else return 0;
+            }
+
+            if (stack.Count == 0)
+                return 1;
+            else
+                return 0;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = "[()](";
+            int answer = Solution.solution(s);
+
+            Console.WriteLine($"{answer}");
+        }
+    }
+}
