6025df1ffa2f1d4fd4ed0be49974965335d1d783
Martin <martin.lippl@pueo.de>
2021-06-27 16:10:17 +0200

    Iniitial commit

---
+++ Iterations/BinaryGap/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections;
+using static System.Console;
+
+namespace BinaryGap
+{
+    class Solution
+    {
+        public static int solution(int N) {
+            int gap = 0;
+            bool rightOne = false;
+
+            for (int i = 0; i < 32; i++) {
+                int oneHot = (int) Math.Pow(2,i); // Corner case 2^31
+                
+                if ((N & oneHot) != 0 ) 
+                {
+                    if (rightOne is false) 
+                        rightOne = true;
+                    else
+                        continue;
+                } 
+                else if ((N & oneHot) == 0 && (rightOne is true)) 
+                {
+                    gap++;
+                }   
+            }
+
+            return gap;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = 473; // = (0001 1101 1001)_2
+            int gapLength = Solution.solution(number);
+            WriteLine("The binary gap has a length of {0}.", gapLength);
+        }
+    }
+}
---
+++ Sorting/Distinct/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using static System.Console;
+
+namespace Distinct
+{
+    class Solution
+    {
+        public static int solution(int[] numbersArray) {
+            int distinctNumbers = 0;
+            Array.Sort(numbersArray);
+            
+            for (int i=0; i < numbersArray.Length; i++)
+            {
+                
+            }
+
+
+            return distinctNumbers;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = new int[] {2, 1, 1, 2, 3, 1};
+            int result = Solution.solution(array);
+
+            WriteLine("There are {0} distinct numbers in the array.", result);
+        }
+    }
+}
