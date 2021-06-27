b8ea6a9524043509ae93bb4d2ac4bf8e8ccb04c3
Martin <martin.lippl@pueo.de>
2021-06-27 16:37:26 +0200

    Finished task, albeit with brute-force solution

--- Sorting/Distinct/Program.cs
+++ Sorting/Distinct/Program.cs
@@ -5,15 +5,23 @@ namespace Distinct
 {
     class Solution
     {
+        
         public static int solution(int[] numbersArray) {
-            int distinctNumbers = 0;
-            Array.Sort(numbersArray);
+            int distinctNumbers = 1;
             
-            for (int i=0; i < numbersArray.Length; i++)
+            if (numbersArray.Length == 0)
+                return 0;
+            
+            Array.Sort(numbersArray); // O(N*log(N))
+            
+            // O(N)
+            for (int i=1; i < numbersArray.Length; i++)
             {
-                
+                if (!numbersArray[i].Equals(numbersArray[i-1]))
+                {
+                    distinctNumbers++;
+                }
             }
-
 
             return distinctNumbers;
         }
@@ -22,10 +30,11 @@ namespace Distinct
     {
         static void Main(string[] args)
         {
-            int[] array = new int[] {2, 1, 1, 2, 3, 1};
+            int[] array = new int[] {2, 1, 1, 2, 7, 3, 1};
             int result = Solution.solution(array);
 
             WriteLine("There are {0} distinct numbers in the array.", result);
+            // WriteLine($"There are {result} distinct numbers in the array.");
         }
     }
 }
