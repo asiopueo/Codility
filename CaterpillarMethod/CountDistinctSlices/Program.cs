c0f7ed5a35cfba07aa0ef7a9fbbb8d7ed482aa6c
Martin <martin.lippl@pueo.de>
2021-06-29 15:01:39 +0200

    Finished, however unclear if this is the optimal solution

--- CaterpillarMethod/CountDistinctSlices/Program.cs
+++ CaterpillarMethod/CountDistinctSlices/Program.cs
@@ -1,12 +1,37 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace CountDistinctSlices
 {
+    class Solution {
+        public static int solution(int M, int[] A) {
+            int counter = 0;
+            HashSet<int> hashSet = new HashSet<int>();
+
+            int end;
+            for (int start = 0; start<A.Length; start++)
+            {
+                end = start;
+                while (end<A.Length && hashSet.Add(A[end]))
+                {
+                    end++;
+                    counter++;
+                }
+                hashSet.Clear();
+            }
+
+            return counter;
+        }
+    }
+
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int M = 6;
+            int[] A = new int[] {3,4,5,5,2};
+            int result = Solution.solution(M, A);
+            Console.WriteLine($"{result}");
         }
     }
 }
