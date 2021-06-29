ef8cd9dd3d33726c476dd1b80df824fb20deed57
Martin <martin.lippl@pueo.de>
2021-06-29 21:17:31 +0200

    Initialized project 'StoneWall' and added boilerplate code

---
+++ StacksAndQueues/StoneWall/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace StoneWall
+{
+    class Solution {
+        public static int solution(int[] H) {
+            return 0;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] H = new int[] {8,8,5,7,9,8,7,4,8};
+            int result = Solution.solution(H);
+            Console.WriteLine($"{result}");
+        }
+    }
+}
