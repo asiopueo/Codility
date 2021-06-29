3e4c4dd8b2b75ed99cff228f94a81ab94494aa41
Martin <martin.lippl@pueo.de>
2021-06-29 14:43:15 +0200

    Established an Ansatz

--- CaterpillarMethod/CountTriangles/Program.cs
+++ CaterpillarMethod/CountTriangles/Program.cs
@@ -2,11 +2,26 @@
 
 namespace CountTriangles
 {
+    class Solution {
+        public static int solution(int[] A) {
+            int counter = 0;
+            Array.Sort(A); // O(N log N)
+
+            int end = A.Length-1;
+            // ...
+
+
+            return counter;
+        }
+    }
+
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int[] A = new int[] {10, 2, 5, 1, 8, 12};
+            int result = Solution.solution(A);
+            Console.WriteLine($"{result}");
         }
     }
 }
