5a39f0130788aa8da21c0926b59bfa65b0fdab2c
Martin <martin.lippl@pueo.de>
2021-06-29 08:28:09 +0200

    Empty project

---
+++ CaterpillarMethod/AbsDistinct/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace AbsDistinct
+{
+    class Solution {
+        public static int solution(int[] A)
+        {
+            return 0;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = new int[] {-5, -3, -1, 0, 3, 6};
+            int result = Solution.solution(array);
+            Console.WriteLine($"{result}");
+        }
+    }
+}
