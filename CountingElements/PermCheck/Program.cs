4e621566495869dbb2571c4c2194d5dacbff49fa
Martin <martin.lippl@pueo.de>
2021-06-30 17:04:05 +0200

    Implemented solution

--- CountingElements/PermCheck/Program.cs
+++ CountingElements/PermCheck/Program.cs
@@ -1,12 +1,42 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace PermCheck
 {
+    class Solution {
+        // Time complexity: O(N)
+        // Space complexity: O(N)
+        public static int solution(int[] A) {
+            HashSet<int> hashSet = new HashSet<int>();
+
+            int i=0;
+            while( i < A.Length && hashSet.Add(A[i]) )
+            {
+                if (hashSet.Count == A.Length) {
+                    // Check if all integers 1..N are contained
+                    int j=1; 
+                    while( j <= A.Length && hashSet.Remove(j) ) {
+                        if (hashSet.Count==0) {
+                            return 1;
+                        }
+                        j++;
+                    }
+                }
+                i++;                
+            }
+
+            return 0;
+        }
+    }
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int[] A = new int[] {4,1,3,2};
+            //int[] A = new int[] {4,1,3};
+
+            int result = Solution.solution(A);
+            Console.WriteLine($"{result}");
         }
     }
 }
