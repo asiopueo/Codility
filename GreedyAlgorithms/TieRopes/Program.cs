29c7b05abba0bc78c6921959a7065d5c146cb18a
Martin <martin.lippl@pueo.de>
2021-07-01 21:49:40 +0200

    Implemented solution (too easy?). However, I still don't have a proof of optimality.

--- GreedyAlgorithms/TieRopes/Program.cs
+++ GreedyAlgorithms/TieRopes/Program.cs
@@ -3,11 +3,21 @@
 namespace TieRopes
 {
     class Solution {
+        // Time complexity: O(N)
+        // Space complexity: O(1)
+        // Proof of optimality: ???
         public static int solution(int K, int[] A) {
             int counter = 0;
 
-            
-
+            int ropeLength = 0;
+            for (int i=0; i<A.Length; i++)
+            {
+                ropeLength += A[i];
+                if (ropeLength >= K) {
+                    counter++;
+                    ropeLength = 0;
+                }
+            }
 
             return counter;
         }
@@ -19,7 +29,7 @@ namespace TieRopes
             int K = 4;
             int[] A = new int[] {1,2,3,4,1,1,3};
             int result = Solution.solution(K,A);
-            Console.WriteLine($"{result}");
+            Console.WriteLine($"The number of ropes with length greater than or equal to {nameof(K)}={K} is: {result}");
         }
     }
 }
