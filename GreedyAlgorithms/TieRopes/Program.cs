ebfab6471eda80fe8bb47febb4370a94ec0d9a45
Martin <martin.lippl@pueo.de>
2021-07-01 21:35:36 +0200

    Some progress

--- GreedyAlgorithms/TieRopes/Program.cs
+++ GreedyAlgorithms/TieRopes/Program.cs
@@ -2,11 +2,24 @@
 
 namespace TieRopes
 {
+    class Solution {
+        public static int solution(int K, int[] A) {
+            int counter = 0;
+
+            
+
+
+            return counter;
+        }
+    }
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int K = 4;
+            int[] A = new int[] {1,2,3,4,1,1,3};
+            int result = Solution.solution(K,A);
+            Console.WriteLine($"{result}");
         }
     }
 }
