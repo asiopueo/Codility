c26c8c12ab65149330b55c95035312ceff7a22ef
Martin <martin.lippl@pueo.de>
2021-07-02 00:35:23 +0200

    Implemented solution without formal proof of optimality

--- GreedyAlgorithms/MaxNonOverlappingSegments/Program.cs
+++ GreedyAlgorithms/MaxNonOverlappingSegments/Program.cs
@@ -5,13 +5,25 @@ namespace MaxNonOverlappingSegments
     class Solution {
         // Since the topic is about greedy algorithms, we simply proceed by iterating from left to right.
         // However, it is in my opinion not evident that this will lead to an optimal solution.
+        // Time complexity: O(N)
+        // Space complexity: O(1)
         public static int solution(int[] A, int[] B) {
             int counter = 0;
+            int currentEnd = int.MinValue;
 
-            for (int i=0; i<A.Length; i++) {
+            if (A.Length == 0 || B.Length == 0 || A.Length != B.Length)
+                return -1;
+
+            for (int i=0; i<A.Length-1; i++) {
+                while(A[i] <= currentEnd) {
+                    i++;
+                }
+                currentEnd = B[i];
                 counter++;
-                
             }
+
+            if (A[A.Length-1]>currentEnd)
+                counter++;
 
             return counter;
         }
