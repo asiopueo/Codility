010603406f00861b9a467dfb562c48d7fac67151
Martin <martin.lippl@pueo.de>
2021-07-03 13:55:56 +0200

    Initialized and implmented project 'PermMissingElem'

---
+++ TimeComplexity/PermMissingElem/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace PermMissingElem
+{
+    class Solution {
+        // Time complexity: O(N)
+        // Space complexity: O(1)
+        public static int solution(int[] A) {
+            int sum = 0;
+            for (int i=0; i<A.Length; i++) {
+                sum += A[i];
+            }
+            return ((A.Length+1)*(A.Length+2)/2 - sum);
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] A = new int[] {2,3,1,5};
+            int result = Solution.solution(A);
+            Console.WriteLine($"{result}");
+        }
+    }
+}
