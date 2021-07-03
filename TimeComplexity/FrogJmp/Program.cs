9b24bed117a3306b5c84f11b1e406cd058028f2d
Martin <martin.lippl@pueo.de>
2021-07-03 13:37:45 +0200

    Initialized project and implemented solution of 'FrogJmp'

---
+++ TimeComplexity/FrogJmp/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace FrogJmp
+{
+    class Solution {
+        public static int solution(int X, int Y, int D) {
+            return (Y-X)%D==0 ? (Y-X)/D : (Y-X)/D+1;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int X = 10;
+            int Y = 85;
+            int D = 30;
+            int result = Solution.solution(X,Y,D);
+            Console.WriteLine($"{result}");
+        }
+    }
+}
