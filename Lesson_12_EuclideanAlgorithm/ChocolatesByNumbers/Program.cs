ee5a409536b30141d874f26d63b1960cf9bc6646
Martin <martin.lippl@pueo.de>
2021-07-04 12:39:43 +0200

    Implemented 'ChocolatesByNumbers'

---
+++ Lesson_12_EuclideanAlgorithm/ChocolatesByNumbers/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ChocolatesByNumbers
+{
+    class Solution {
+        // Time complexity: O(N)
+        // Space complexity: O(N)
+        public static int solution(int N, int M) {
+            HashSet<int> hashSet = new HashSet<int>(N);
+            int counter = 0;
+
+            int i = 0;
+            while (hashSet.Add(i)) {
+                counter++;
+                i = (i+M)%N;
+            }
+
+            return counter;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int N = 10;
+            int M = 4;
+            int result = Solution.solution(N,M);
+            Console.WriteLine($"{result}");
+        }
+    }
+}
