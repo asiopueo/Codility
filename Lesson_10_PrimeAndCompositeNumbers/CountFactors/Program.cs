8d3fd5e7b95e7744b35125c4a7b4db5bc45aa072
Martin <martin.lippl@pueo.de>
2021-07-07 16:07:58 +0200

    Implemented brute-force and optimal solutions

--- Lesson_10_PrimeAndCompositeNumbers/CountFactors/Program.cs
+++ Lesson_10_PrimeAndCompositeNumbers/CountFactors/Program.cs
@@ -2,11 +2,44 @@
 
 namespace CountFactors
 {
+    class Solution {
+        // Time complexity: O(N)
+        // Space complexity: O(1)
+        public static int solutionBF(int N) {
+            int counter = 0;
+            
+            for (int i=1; i<N+1; i++) {
+                if (N%i==0)
+                    counter++;
+            }
+
+            return counter;
+        }
+
+        // Time complexity: O(sqrt(N))
+        // Space complexity: O(1)
+        public static int solution(int N) {
+            int sqrtN = (int) Math.Sqrt(N);
+            int counter = 0;
+
+            for (int i=1; i < sqrtN+1; i++) {
+                if (N%i==0)
+                    counter += 2;
+            }
+
+            return counter;
+        }
+    }
+
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int N = 24;
+
+            int result = Solution.solution(N);
+
+            Console.WriteLine($"{result}");
         }
     }
 }
