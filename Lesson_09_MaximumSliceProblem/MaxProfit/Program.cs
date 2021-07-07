455d2422d8cdea68d16ce497b3bf12c791b6b518
Martin <martin.lippl@pueo.de>
2021-07-07 16:54:32 +0200

    Initialized and implemented optimal solution for 'MaxProfit'

---
+++ Lesson_09_MaximumSliceProblem/MaxProfit/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace MaxProfit
+{
+    class Solution {
+        // Time complexity: O(N)
+        // Space complexity: O(1)
+        public static int solution(int[] A) {
+            int minPrice = int.MaxValue;
+            int maxProfit = 0;
+
+            foreach (int spot in A) {
+                minPrice = Math.Min(minPrice, spot);
+                maxProfit = Math.Max(maxProfit, spot-minPrice);
+            }
+            
+            return maxProfit; // 356
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] A = new int[] {23171,21011,21123,21366,21013,21367};
+            int result = Solution.solution(A);
+            Console.WriteLine($"{result}");
+        }
+    }
+}
