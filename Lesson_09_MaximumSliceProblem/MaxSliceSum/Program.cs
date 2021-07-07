3f7b031ed6b1dc858fc75846ffcebadef8fc2e76
Martin <martin.lippl@pueo.de>
2021-07-07 17:26:44 +0200

    Initialized and implemented optimal solution for 'MaxSliceSum'

---
+++ Lesson_09_MaximumSliceProblem/MaxSliceSum/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace MaxSliceSum
+{
+    class Solution {
+        // Time complexity: O(N)
+        // Space complexity: O(1)
+        public static int solution(int[] A) {
+            int maxSliceSum = int.MinValue;
+            int currentSum = 0;
+
+            foreach (int element in A) {
+                currentSum = Math.Max(element, currentSum+element);
+                maxSliceSum = Math.Max(maxSliceSum, currentSum);
+            }
+            
+            return maxSliceSum; // 5
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] A = new int[] {3,2,-6,4,0};
+
+            int result = Solution.solution(A);
+
+            Console.WriteLine($"{result}");
+        }
+    }
+}
