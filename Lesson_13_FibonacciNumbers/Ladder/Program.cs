d100e1dfa10708c0559c77b189f3e30d6af39fbd
Martin <martin.lippl@pueo.de>
2021-07-07 17:46:15 +0200

    Initialized project 'Ladder' and added boiler plate code

---
+++ Lesson_13_FibonacciNumbers/Ladder/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Ladder
+{
+    class Solution {
+        public static int[] solution(int[] A, int[] B) {
+            return new int[] {};
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] A = new int[] {4,4,5,5,1};
+            int[] B = new int[] {3,2,4,3,1};
+            int[] result = Solution.solution(A, B);
+            Console.WriteLine($"{result}");
+        }
+    }
+}
