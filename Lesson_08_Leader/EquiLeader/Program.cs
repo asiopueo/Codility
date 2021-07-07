d7b45bd6424503d5a6d324fdd449e5af9d23e7a5
Martin <martin.lippl@pueo.de>
2021-07-07 14:51:32 +0200

    Some boilerplate code

--- Lesson_08_Leader/EquiLeader/Program.cs
+++ Lesson_08_Leader/EquiLeader/Program.cs
@@ -2,11 +2,29 @@
 
 namespace EquiLeader
 {
+    class Solution {
+        public static int solution(int[] A) {
+            int counter = 0;
+            int leaderA, leaderB;
+
+            for (int i=0; i<A.Length; i++) {
+                
+                if (leaderA == leaderB)
+                    counter++;
+            }
+
+            return -1;
+        }
+    }
+
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int[] A = new int[] {4,3,4,4,4,2};
+            int result = Solution.solution(A);
+
+            Console.WriteLine($"The number of equi leaders is: {result}");
         }
     }
 }
