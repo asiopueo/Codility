7a9fed12fedb36483f9c8c2bbad57e0a531dc813
Martin <martin.lippl@pueo.de>
2021-07-07 14:28:04 +0200

    Implemented optimal solution for 'PassingCars'

--- Lesson_05_PrefixSums/PassingCars/Program.cs
+++ Lesson_05_PrefixSums/PassingCars/Program.cs
@@ -6,19 +6,16 @@ namespace PassingCars
 {
     class Solution 
     {
-        // This seems to be the brute-force solution, O(N^2) time complexity.
-        public static int solution(int[] A) 
+        // Brute force solution: O(N^2) time complexity, O(1) space complexity
+        public static int solutionBF(int[] A) 
         {
             int counter = 0;
 
             for (int i=0; i<A.Length; i++)
             {
-                if (A[i] == 0)
-                {
-                    for (int j=i+1; j<A.Length; j++)
-                    {
-                        if (A[j] == 1)
-                        {
+                if (A[i] == 0) {
+                    for (int j=i+1; j<A.Length; j++) {
+                        if (A[j] == 1) {
                             counter++;
                         }
                     }
@@ -27,13 +24,32 @@ namespace PassingCars
 
             return counter;
         }
+
+        // Time complexity: O(N)
+        // Space complexity: O(1)
+        public static int solution(int[] A)
+        {
+            int counter = 0;
+            int zeroMultiplier = 0;
+            
+            for(int i=0; i<A.Length; i++)
+                if (A[i]==0)
+                    zeroMultiplier++;
+                else if (A[i]==1)
+                    counter += zeroMultiplier;
+
+                if (counter > 1e9)
+                    return -1;
+
+            return counter;
+        }
     }
     class Program
     {
         static void Main(string[] args)
         {
-            //int[] array = new int[] {0, 1, 0, 1, 1};
-            int[] array = new int[] {0, 1, 0, 1, 1, 0, 1};
+            int[] array = new int[] {0, 1, 0, 1, 1};
+            //int[] array = new int[] {0, 1, 0, 1, 1, 0, 1};
 
             int result = Solution.solution(array);
 
