0c9d9f610a85876318cbb950f4235e46a0ca1d49
Martin <martin.lippl@pueo.de>
2021-07-08 14:40:00 +0200

    Implemented CountDiv-solution in O(1)

--- Lesson_05_PrefixSums/CountDiv/Program.cs
+++ Lesson_05_PrefixSums/CountDiv/Program.cs
@@ -2,11 +2,29 @@
 
 namespace CountDiv
 {
+    class Solution {
+        // Time complexity: O(1)
+        // Space complexity: O(1)
+        public static int solution(int A, int B, int K) {
+            if ( (B-A)%K == 0 )
+                return (B-A)/K;
+            else
+                return (B-A)/K + 1 ;
+        }
+
+        public static int solution2(int A, int B, int K) {
+            return (B-A)/K + ((B-A)%K==0 ? 0 : 1);
+        }
+    }
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int A=6, B=11, K=2; // 3 expected
+            //int A=7, B=31, K=5; // 5 expected
+            //int A=16, B=342, K=17; // 20 expected
+            int result = Solution.solution2(A,B,K);
+            Console.WriteLine($"{result}");
         }
     }
 }
