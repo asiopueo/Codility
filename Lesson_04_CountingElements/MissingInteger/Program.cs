79d3734fde6b3e4d227582b12859cfdb888abc10
Martin <martin.lippl@pueo.de>
2021-07-08 13:56:23 +0200

    Implemented 'MissingInteger'

--- Lesson_04_CountingElements/MissingInteger/Program.cs
+++ Lesson_04_CountingElements/MissingInteger/Program.cs
@@ -1,12 +1,37 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace MissingInteger
 {
+    class Solution {
+        // Time complexity: O()
+        // Space complexity: O(N)
+        public static int solution(int[] A) {
+            HashSet<int> hashSet = new HashSet<int>();
+            int maxElement = 1;
+
+            foreach (int entry in A) {
+                hashSet.Add(entry);
+                maxElement = Math.Max(maxElement, entry);
+            }
+
+            for(int i=1; i<=Math.Max(1, maxElement); i++)
+                if( !hashSet.Contains(i) )
+                    return i;
+            
+            return maxElement+1;
+        }
+    }
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int[] A = new int[] {1,3,6,4,1,2}; // 5
+            //int[] A = new int[] {1,2,3}; // 4
+            //int[] A = new int[] {-1,-3}; // 1
+            int result = Solution.solution(A);
+            Console.WriteLine($"{result}");
         }
     }
 }
