f48062640ccdd87a70e4fa18f6d29fbae5b9a633
Martin <martin.lippl@pueo.de>
2021-07-09 16:16:39 +0200

    Implemented solution which works fine. Corner cases?

--- Lesson_09_MaximumSliceProblem/MaxDoubleSliceSum/Program.cs
+++ Lesson_09_MaximumSliceProblem/MaxDoubleSliceSum/Program.cs
@@ -4,30 +4,30 @@ using System.Collections.Generic;
 namespace MaxDoubleSliceSum
 {
     class Solution {
+        // This solution utilizes Kudane's algorithm
         // Time complexity: O(N)
         // Space complexity: O(N)
         public static int solution(int[] A) 
         {
-            int currentSum = 0;
-            int maxSliceSum = int.MinValue;
-
             int[] Sum1 = new int[A.Length]; // Max sum ending at index i
             int[] Sum2 = new int[A.Length]; // Max sum starting at index i
 
-            for (int i=1; i<A.Length; i++) {
-                currentSum = Math.Max(A[i], currentSum+A[i]);
-                maxSliceSum = Math.Max(maxSliceSum, currentSum);
+            int currentSum = 0;
+            for (int i=1; i<A.Length-1; i++) {
+                currentSum = Math.Max(currentSum+A[i], 0);
+                Sum1[i] = currentSum;
             }
-
-            for (int i=A.Length-1; i>0; i--) {
-                currentSum = Math.Max(A[i], currentSum+A[i]);
-                maxSliceSum = Math.Max(maxSliceSum, currentSum);
+            
+            currentSum = 0;
+            for (int i=A.Length-2; i>0; i--) {
+                currentSum = Math.Max(currentSum+A[i], 0);
+                Sum2[i] = currentSum;
             }
 
             int maxDoubleSliceSum = int.MinValue;
 
-            for (int i=1; i<A.Length-1; i++)
-                maxDoubleSliceSum = Math.Max(maxDoubleSliceSum, Sum1[i-1]+Sum2[i+1]);
+            for (int i=2; i<A.Length-2; i++)
+                maxDoubleSliceSum = Math.Max(maxDoubleSliceSum, Sum1[i-1]+Sum2[i+1] );
 
             return maxDoubleSliceSum; 
         }
