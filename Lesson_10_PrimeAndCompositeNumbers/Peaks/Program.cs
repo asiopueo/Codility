3f9d3b73186a9a0289bbc77498cf440320c4c843
Martin <martin.lippl@pueo.de>
2021-07-09 17:36:19 +0200

    Iniitlaized project 'Peaks'

---
+++ Lesson_10_PrimeAndCompositeNumbers/Peaks/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Peaks
+{
+    class Solution {
+        // Time complexity: O()
+        // Space complexity: O()
+        public static int solution(int[] A) 
+        {
+            List<int> peaks = new List<int>();
+
+            for (int i=1; i<A.Length; i++)
+                if(A[i-1]<A[i] && A[i+1]<A[i]) 
+                    peaks.Add(i);
+
+            foreach(var item in peaks)
+                Console.Write($"{item}, ");                
+
+            for (int i=peaks[0]+1; i<=peaks[1]; i++) {
+                if 
+                // else
+                // return 0;
+                    
+            }
+
+            return int.MinValue; // expected: 3
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] A = new int[] {1,2,3,4,3,4,1,2,3,4,6,2}; // peaks 3,5,10
+            int result = Solution.solution(A);
+            Console.WriteLine($"{result}");
+        }
+    }
+}
