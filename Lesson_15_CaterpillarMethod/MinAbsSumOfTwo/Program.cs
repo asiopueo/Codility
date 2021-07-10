5c04f1b2872c7b75b8a69ac517229cf04e874725
Martin <martin.lippl@pueo.de>
2021-07-10 21:33:32 +0200

    Initialized 'MinAbsSumOfTwo'

---
+++ Lesson_15_CaterpillarMethod/MinAbsSumOfTwo/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace MinAbsSumOfTwo
+{
+    class Solution {
+        public static int solution(int[] A) {
+            int sum ;
+
+            for (int i=0; i<A.Length; i++) {
+                
+            }
+
+            return sum;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] A = new int[] {1,4,-3}; // expected: |4+(-3)|=1
+            //int[] A = new int[] {-8,4,5,-10,3}; // expected: |(-8)+5|=3
+            int result = Solution.solution(A);
+            Console.WriteLine($"{result}");
+        }
+    }
+}
