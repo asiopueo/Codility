de860a73731296c0bd831f0ce70844e9a49924b2
Martin <martin.lippl@pueo.de>
2021-07-10 22:17:12 +0200

    Implemented efficient solution

--- Lesson_15_CaterpillarMethod/MinAbsSumOfTwo/Program.cs
+++ Lesson_15_CaterpillarMethod/MinAbsSumOfTwo/Program.cs
@@ -3,14 +3,31 @@
 namespace MinAbsSumOfTwo
 {
     class Solution {
+        // Brute-force solution: O(N**2)
+        // Time complexity: O(N*log(N))
+        // Space complexity: O(1)
         public static int solution(int[] A) {
-            int sum ;
+            int minSum = int.MaxValue;
+            int absSum = int.MaxValue;
+            int sum = 0;
+            Array.Sort(A); // O(N*log(N))
 
-            for (int i=0; i<A.Length; i++) {
-                
+            int left = 0;
+            int right = A.Length-1;
+            
+            while (left <= right) { // O(N)
+                sum = A[left] + A[right];
+                absSum = Math.Abs(sum);
+                minSum = Math.Min(minSum, absSum);
+
+                if (sum <= 0)
+                    left++;
+                else
+                    right--;
+
             }
 
-            return sum;
+            return minSum;
         }
     }
 
@@ -18,8 +35,9 @@ namespace MinAbsSumOfTwo
     {
         static void Main(string[] args)
         {
-            int[] A = new int[] {1,4,-3}; // expected: |4+(-3)|=1
-            //int[] A = new int[] {-8,4,5,-10,3}; // expected: |(-8)+5|=3
+            //int[] A = new int[] {1,4,-3}; // expected: |4+(-3)|=1
+            int[] A = new int[] {-8,4,5,-10,3}; // expected: |(-8)+5|=3
+            // Sorted: A = {-10,-8,3,4,5}
             int result = Solution.solution(A);
             Console.WriteLine($"{result}");
         }
